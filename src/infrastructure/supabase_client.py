d90be39edc76b7cdaef7c15435e4f5c8ff9ce8a2
guilherme-hl1ma <guihlima@outlook.com>
2025-06-05 10:32:26 -0300

    feat:aplicar padrão de projeto Singleton para a instancia do Supabase

--- src/infrastructure/supabase_client.py
+++ src/infrastructure/supabase_client.py
@@ -1,7 +1,14 @@
 from supabase import create_client
 
+SUPABASE_URL = "https://ypypuvchmodptwbvbwtb.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlweXB1dmNobW9kcHR3YnZid3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDYwMjQsImV4cCI6MjA2MDQ4MjAyNH0.BvGZTOPigHlFwXTX9I81NWRyRz_V1jDU800aAtju57w"
 
-SUPABASE_URL="https://ypypuvchmodptwbvbwtb.supabase.co"
-SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlweXB1dmNobW9kcHR3YnZid3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDYwMjQsImV4cCI6MjA2MDQ4MjAyNH0.BvGZTOPigHlFwXTX9I81NWRyRz_V1jDU800aAtju57w"
+class SupabaseClientSingleton:
+    _instance = None
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = create_client(SUPABASE_URL, SUPABASE_KEY)
+        return cls._instance
+
+supabase = SupabaseClientSingleton()
